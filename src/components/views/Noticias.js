50f129963614dff7a00b6db36a1c8b9b681cc697
Omar Leiva <oleiva@ticteams.com>
2021-03-10 17:39:03 -0500

    Cambios sobre pantallas Omar Leiva

--- src/components/views/Noticias.js
+++ src/components/views/Noticias.js
@@ -1,16 +1,136 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Body,
+  Text,
+  Icon,
+} from "native-base";
+import DefaultStyles from "styles/defaultStyles";
+import HeaderMolecules from "molecules/Header";
+import { size, map } from "lodash";
+import moment from "moment/moment";
 
-// import { Header } from "molecules";
-import Header from "../molecules/Header";
+const data = [
+  {
+    id: "XXXX101429392121q",
+    image: require("assets/servicios-tv.png"),
+    titulo: "Nueva Prociones en Más Móvil",
+    descripcion:
+      "Aproveche las nuevas promociones de más móvil, en el mes de enero, solo por este mes estaremos brindando planes totalmente personalizados desde 20 dólares en adelante",
+    fecha: new Date(),
+  },
+  {
+    id: "XXXX101429392121A",
+    image: require("assets/navegar.png"),
+    titulo: "¿Qué es un plan de datos?",
+    descripcion:
+      "Aproveche las nuevas promociones de más móvil, en el mes de enero, solo por este mes estaremos brindando planes totalmente personalizados desde 20 dólares en adelante",
+    fecha: new Date(),
+  },
+  {
+    id: "XXXX101429392121b",
+    image: require("assets/5g.png"),
+    titulo: "¿Sábes que es 5G?",
+    descripcion:
+      "Aproveche las nuevas promociones de más móvil, en el mes de enero, solo por este mes estaremos brindando planes totalmente personalizados desde 20 dólares en adelante",
+    fecha: new Date(),
+  },
+];
 
+console.log(data);
+
+moment.locale("es");
 const Noticias = () => {
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
-      <Header />
-      <Text style={{ fontSize: 50 }}>Noticias</Text>
+      <HeaderMolecules />
+      <View style={styles.headerfilter}>
+        <Text
+          style={{
+            ...DefaultStyles.boldOrangeText,
+            alignSelf: "center",
+            marginTop: 10,
+          }}
+        >
+          Noticias
+        </Text>
+      </View>
+
+      <Container style={styles.containermain}>
+        <Content>
+          {size(data) > 0 &&
+            map(data, (item) => (
+              <Card style={styles.card} key={item.id}>
+                <View style={styles.rowinside}>
+                  <Image source={item.image} style={styles.imagen} />
+                  <View style={{ width: "60%" }}>
+                    <Text style={styles.text}>{item.titulo}</Text>
+                    <Text style={styles.date}>
+                      {moment(item.date).format("l")}
+                    </Text>
+                  </View>
+                </View>
+              </Card>
+            ))}
+        </Content>
+      </Container>
     </View>
   );
+};
+
+const styles = {
+  headerbox: {
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  headerfilter: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  rowinside: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+    paddingHorizontal: 40,
+    marginVertical: 5,
+  },
+  card: {
+    paddingVertical: 20,
+    width: "95%",
+    alignSelf: "center",
+    borderRadius: 20,
+  },
+  text: {
+    fontSize: 20,
+    fontFamily: "FuturaPTBook",
+    color: "#000",
+    marginLeft: 10,
+  },
+  containermain: {
+    marginTop: 20,
+  },
+  icon: {
+    color: "#FF4713",
+  },
+  imagen: {
+    width: 85,
+    height: 102,
+  },
+  date: {
+    marginLeft: 15,
+    marginTop: 10,
+    fontSize: 20,
+    fontFamily: "FuturaPTBook",
+    color: "#981D97",
+    marginLeft: 10,
+    fontWeight: "bold",
+  },
 };
 
 export default Noticias;
--- src/components/views/RegistrarChip.js
+++ src/components/views/RegistrarChip.js
@@ -1,16 +1,95 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Dimensions, ScrollView, StyleSheet } from "react-native";
+import { Input, Item, Button, Icon } from "native-base";
+import { CustomHeader } from "molecules";
+import DefaultStyles from "styles/defaultStyles";
 
-// import { Header } from "molecules";
-import Header from "../molecules/Header";
+let today = new Date().toISOString().slice(0, 10);
+const screenWidth = Dimensions.get("window").width;
 
 const RegistrarChip = () => {
+  const [state, setstate] = useState("");
   return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
-      <Header />
-      <Text style={{ fontSize: 50 }}>Registrar Chip</Text>
+    <View style={DefaultStyles.container}>
+      <CustomHeader />
+      <Text
+        style={{
+          ...DefaultStyles.boldOrangeText,
+          alignSelf: "center",
+          marginTop: 10,
+        }}
+      >
+        Escanea el Bar Code del Chip
+      </Text>
+      <Icon type="Ionicons" name="barcode-outline" style={styles.iconblack} />
+      <View style={styles.internalcontainer}>
+        <Icon
+          type="Ionicons"
+          name="camera"
+          style={{ fontSize: 80, color: "#707070" }}
+        />
+        <Text
+          style={{
+            ...DefaultStyles.boldOrangeText,
+            alignSelf: "center",
+            textAlign: "center",
+          }}
+        >
+          Presione la camara para escanear el código
+        </Text>
+
+        <View style={styles.centering}>
+          <Item rounded style={{ borderRadius: 10, marginBottom: 5 }}>
+            <Input
+              placeholder="Ingrese manualmente"
+              placeholderTextColor="#FF4713"
+              style={{
+                ...DefaultStyles.normalOrangeText,
+                textAlign: "center",
+              }}
+              keyboardType={"decimal-pad"}
+              onChangeText={(text) => {
+                setstate(text);
+              }}
+              value={state}
+            />
+          </Item>
+        </View>
+        <Button
+          full
+          rounded
+          style={styles.btn}
+          onPress={() => console.log("Registrar")}
+        >
+          <Text style={DefaultStyles.buttonText}>Registrar Chip</Text>
+        </Button>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  iconblack: {
+    fontSize: 30,
+    color: "#000000",
+    marginTop: 5,
+    alignSelf: "flex-end",
+    marginRight: 20,
+  },
+  internalcontainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btn: {
+    width: "80%",
+    alignSelf: "center",
+  },
+  centering: {
+    marginTop: 20,
+    width: "80%",
+    marginVertical: 30,
+  },
+});
 
 export default RegistrarChip;
